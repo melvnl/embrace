6b818a98b0cc03b0996f75e4ca9de1082c5f08f8
Vendly <vendlytan@gmail.com>
2022-06-14 12:48:22 +0700

    profile section updated

---
+++ MC-2/Cell/ProfilePostCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfilePostCell.swift
+//  MC-2
+//
+//  Created by Vendly on 13/06/22.
+//
+
+import UIKit
+
+class ProfilePostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
