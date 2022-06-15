6b586f3c3857896b3719bc868a917874d26f90d5
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-06-15 20:55:08 +0700

    last 15 june

---
+++ MC-2/View Controllers/AddNewForumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewForumViewController.swift
+//  MC-2
+//
+//  Created by Stefanus Hermawan Sebastian on 15/06/22.
+//
+
+import UIKit
+
+class AddNewForumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
