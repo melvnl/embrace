e18675331fdfa617e5ffaa3e6d37404080a29bc1
himelv <63998835+melvnl@users.noreply.github.com>
2022-06-21 10:15:32 +0700

    Feature/forum (#7)
    * design awal
    
    * UI add new forum
    
    * last 15 june
    
    * frontend forum done (NATIVE)
    
    * udah berhasil ke save, cuman masih ke threads (gw mau ubah ke forum)
    
    * udah berhasil ke save di collection forum
    
    * last
    
    * save to firestore forum done
    
    * create new forum done
    
    * Feature/forum (#7)
    
    * design awal
    
    * UI add new forum
    
    * last 15 june
    
    * frontend forum done (NATIVE)
    
    * udah berhasil ke save, cuman masih ke threads (gw mau ubah ke forum)
    
    * udah berhasil ke save di collection forum
    
    * last
    
    * save to firestore forum done
    
    * create new forum done
    
    Co-authored-by: melvnl <melvinlim2000@gmail.com>
    
    Co-authored-by: Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
    Co-authored-by: Stefanus Hermawan Sebastian <79867790+stefanushermawan2101@users.noreply.github.com>

---
+++ MC-2/Forum/HomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeViewController.swift
+//  MC-2
+//
+//  Created by Stefanus Hermawan Sebastian on 07/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var addNewForumButton: UIButton!
+    
+    @IBOutlet weak var forumSearchBar: UISearchBar!
+    
+    @IBOutlet weak var forumTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapNewNote() {
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "NewForum") as? AddNewForumViewController else {
+            return
+        }
+        
+        vc.completion = {
+            newEntry in
+            self.navigationController?.popToRootViewController(animated: true)
+            forumRepo.createForum(entry: newEntry)
+                    
+            self.forumTableView.isHidden = false
+        }
+        
+        vc.currEntry = nil
+        vc.isEditingEntry = false
+        
+        vc.title = "Buat Forum"
+        
+        setBackBarItem()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+
+
+}
