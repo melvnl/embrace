79de827fc9208d9883d226444973991c2f198c31
melvnl <melvinlim2000@gmail.com>
2022-06-23 02:46:23 +0700

    feat: add temporary topic detail screen

--- MC-2/View Controllers/Topic/TopicDetailController.swift
+++ MC-2/View Controllers/Topic/TopicDetailController.swift
@@ -14,6 +14,12 @@ class TopicDetailController: UIViewController {
     @IBOutlet weak var topicTitle: UILabel!
     @IBOutlet weak var backBtn: UIButton!
     
+    @IBOutlet weak var headerImg: UIImageView!
+    @IBOutlet weak var headerTitle: UILabel!
+    @IBOutlet weak var headerDesc: UILabel!
+    
+    let placeholderAvatar: String =  "https://firebasestorage.googleapis.com/v0/b/embrace-mini-challenge-2.appspot.com/o/avatar.png?alt=media&token=228d6d5e-53b2-461d-886f-90889981a393"
+    
     var categoryDocId: String = "";
     var categoryTitle: String = "";
     var categorySub: String = "";
@@ -32,12 +38,25 @@ class TopicDetailController: UIViewController {
         
         topicTitle.text = categoryTitle;
         
-        print("test")
-        print(categoryTitle)
-        print(categoryDocId)
+        let docHeader = db.collection("forum").document(categoryDocId).getDocument { (docSnapshot, error) in
+            if let doc = docSnapshot {
+                let title = doc.get("category") as? String ?? ""
+                let desc = doc.get("desc") as? String ?? ""
+                let imgURL_ = URL(string: doc.get("bigThumbail") as? String ??  self.placeholderAvatar)!
+    
+                
+                self.headerTitle.text = title
+                self.topicTitle.text = title
+                self.headerDesc.text = desc
+                self.headerImg.load(url: imgURL_)
+            } else {
+                if let error = error {
+                    print(error)
+                }
+            }
+        }
         
         let docRef = db.collection("forum").document(categoryDocId).collection(categorySub)
-
         docRef.getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
