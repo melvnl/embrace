b5feb4b1a70139fdc637de138e2b234db1ee57b6
Uto <daffamadeo@gmail.com>
2022-06-23 10:12:28 +0700

    Update: Show image journal list (#11)

--- MC-2/Journal/EntryCell.swift
+++ MC-2/Journal/EntryCell.swift
@@ -7,9 +7,11 @@ class EntryCell: UITableViewCell {
     @IBOutlet weak var desc: UITextView!
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var moodImage: UIImageView!
+    @IBOutlet weak var journalImage: UIImageView!
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
         layer.cornerRadius = 10
         layer.masksToBounds = true
         self.contentView.alpha = 0
