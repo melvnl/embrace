76f9d0aff050b7255e05c69e4ec9b242c30e1d44
Uto <daffamadeo@gmail.com>
2022-06-29 18:23:39 +0700

    update: implement dispatch in post comment

--- MC-2/View Controllers/Topic/Comment/CommentCell.swift
+++ MC-2/View Controllers/Topic/Comment/CommentCell.swift
@@ -11,12 +11,19 @@ class CommentCell: UITableViewCell {
     
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var name: UILabel!
-    
     @IBOutlet weak var value: UITextView!
     @IBOutlet weak var avatar: UIImageView!
     
     var forumId = ""
     
-    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        avatar.layer.cornerRadius = 20
+        avatar.clipsToBounds = true
+        
+        self.contentView.alpha = 0
+        UIView.animate(withDuration: 1, animations: { self.contentView.alpha = 1 })
+    }
     
 }
